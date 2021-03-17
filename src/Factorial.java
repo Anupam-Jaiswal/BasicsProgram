0c41c5cef146443b1e704509d6d9e238f381c9ef
Anupam-Jaiswal <jaiswal.anupam2013@gmail.com>
2021-03-17 12:03:41 +0530

    first commit

---
+++ src/Factorial.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Factorial
+{
+	public static void main (String args[])
+	{
+		Scanner kb = new Scanner(System.in);
+		System.out.println("Enter the number");
+		int n = kb.nextInt();
+		int fact = 1;
+		while (n > 0)
+		{
+			fact = fact * n;
+			n--;
+		}
+		System.out.println("factorial is " +fact);
+	}
+}
